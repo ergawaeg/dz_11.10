2d6a38b46cf7ada8989aee42ffc389637386f635
ergawaeg <sinitcinanton7@gmail.com>
2025-12-09 16:29:51 +0500

    ////

--- dz_11.11/FileName.cpp
+++ dz_11.11/FileName.cpp
@@ -34,12 +34,56 @@ void Add()
 	int u;
 	std::cin >> u;
 	arr2[5] = u;
-	for (size_t i = 0; i < 6; i++)
+	std::swap(arr2, arr);
+	size++;
+	for (size_t i = 0; i < size; i++)
 	{
-		std::cout << arr2[i] << " ";
+		std::cout << arr[i] << " ";
 	}
 	std::cout << "\n";
 	delete[] arr2;
+}
+void Addin()
+{
+	int* arr1 = new int[size + 1];
+	int u, p;
+	std::cout << "для вставки выберете индекс 1 - 5 и число - ";
+	std::cin >> u;
+	std::cin >> p;
+	for (size_t i = 0; i < size + 1; i++)
+	{
+		if (i == u - 1)
+		{
+			arr1[i] = p;
+		}
+		else
+		{
+			arr1[i] = arr[i];
+		}
+	}
+	std::swap(arr1, arr);
+	delete[] arr1;
+}
+void Deletein()
+{
+	int* arr1 = new int[size - 1];
+	int u, p;
+	std::cout << "для удаления выберете индекс 1 - 5 - ";
+	std::cin >> u;
+	for (size_t i = 0; i < size - 1; i++)
+	{
+		if (i == u - 1 && u != size)
+		{
+			arr1[i] = arr[i + 1];
+		}
+		else
+		{
+			arr1[i] = arr[i];
+		}
+	}
+	size--;
+	std::swap(arr1, arr);
+	delete[] arr1;
 }
 
 
@@ -50,8 +94,11 @@ int main()
 	SetConsoleOutputCP(1251);
 	Arr();
 	Cout();
-	Add();
-
+	/*Add();*/
+	Addin();
+	Cout();
+	Deletein();
+	Cout();
 
 
 
